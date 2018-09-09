0b0dc25bd902e2ebaabe406be98d626488c03d6c
vmm86 <m.vasilyev86@gmail.com>
2018-09-09 23:52:13 +0300

    Update routes module with some mock user object

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет, Мир!"
+    user = {'username': 'miguel'}
+    return """
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, """ + user['username'] + """!</h1>
+    </body>
+</html>"""
