196a366b6e2be9c03e33604906c021efe85b086c
vmm86 <m.vasilyev86@gmail.com>
2018-09-09 23:56:42 +0300

    Move HTML template to separate self-titled directory

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,16 +1,10 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'miguel'}
-    return """
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, """ + user['username'] + """!</h1>
-    </body>
-</html>"""
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
