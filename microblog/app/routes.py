1368f38b2497a28f3f50d2610a099c088fd166e9
vmm86 <m.vasilyev86@gmail.com>
2018-09-10 23:25:51 +0300

    Add basic login form

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from . import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -22,3 +23,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
