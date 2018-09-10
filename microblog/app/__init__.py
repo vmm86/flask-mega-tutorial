521ca5debf1ac16959ac07bd0a341ea58a396dea
vmm86 <m.vasilyev86@gmail.com>
2018-09-10 23:52:07 +0300

    Add basic SQLite database support

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from . import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from . import routes, models
 
 """
 Сценарий создает объект приложения как экземпляр класса Flask, импортированного из пакета flask.
