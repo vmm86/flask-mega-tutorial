d58439edee68c9d7c7c91d54b119cfacc24de9a4
vmm86 <m.vasilyev86@gmail.com>
2018-09-10 23:31:17 +0300

    Update login form

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from . import app
-from app.forms import LoginForm
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -25,7 +25,13 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data,
+            form.remember_me.data)
+        )
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
