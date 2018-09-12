c0b3fea0ff7713b4f9c72a9e6ae672d0591f65d8
vmm86 <m.vasilyev86@gmail.com>
2018-09-13 00:05:53 +0300

    Add login functionality

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
+from flask_login import UserMixin
 from werkzeug.security import (
     generate_password_hash,
     check_password_hash
 )
 
-from app import db
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -23,6 +24,11 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Post(db.Model):
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, flash, redirect, url_for
+from flask import flash, render_template, redirect, request, url_for
+from flask_login import (
+    current_user,
+    login_required, login_user, logout_user
+)
+from werkzeug.urls import url_parse
 
 from . import app
+from app.models import User
 from .forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Эльдар Рязанов'}
     posts = [
@@ -22,16 +29,28 @@ def index():
             'body': 'Какая гадость эта ваша заливная рыба!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data,
-            form.remember_me.data)
-        )
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
