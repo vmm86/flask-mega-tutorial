a2d974cdb064d9319c196858221de50c36bb96e0
vmm86 <m.vasilyev86@gmail.com>
2018-09-12 23:46:37 +0300

    Add basic login manager

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+login = LoginManager(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
