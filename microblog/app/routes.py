eb5e92b65882de4b70548f941da8f63c3e7f2f52
vmm86 <m.vasilyev86@gmail.com>
2018-09-13 00:21:19 +0300

    Add registration functionality

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,6 +6,8 @@ from flask_login import (
 from werkzeug.urls import url_parse
 
 from . import app
+from app import db
+from app.forms import RegistrationForm
 from app.models import User
 from .forms import LoginForm
 
@@ -54,3 +56,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
